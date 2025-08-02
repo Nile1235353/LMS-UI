21f20c6e2447951a222f35431b9e34a581a3b0ef
Nile1235353 <nilenilewailiberalstudies@gmail.com>
2025-08-02 09:21:47 +0630

    home UI

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,12 +7,55 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-  showConfirm: boolean = false;
-  deleteUserId: string | null = null;
+showModal = false;
+selectedVideo: any = null;
+searchTerm: string = '';
+selectedCategory: string = 'All';
 
-  askDeleteUser() {
-    // this.deleteUserId = id;
-    this.showConfirm = true;
-    // console.log("Asking to delete user with ID:", id);
+videos = [
+  {
+    title: 'Hello Program',
+    url: 'https://youtu.be/TJizaGqCQuI?t=1',
+    thumbnail: 'https://img.youtube.com/vi/f3kgzgxRXJg/hqdefault.jpg',
+    category: 'Tutorial'
+  },
+  {
+    title: 'Fishing Woman in the Mountains',
+    url: 'https://www.youtube.com/embed/WCpVoC847Qs',
+    thumbnail: 'https://img.youtube.com/vi/WCpVoC847Qs/hqdefault.jpg',
+    category: 'Nature'
+  },
+  {
+    title: 'Mountain River Fishing',
+    url: 'https://www.youtube.com/embed/grMPM0JhAcQ',
+    thumbnail: 'https://img.youtube.com/vi/grMPM0JhAcQ/hqdefault.jpg',
+    category: 'Nature'
+  },
+  {
+    title: 'Nature Calm Trip',
+    url: 'https://www.youtube.com/embed/ZBbXUR9PVzw',
+    thumbnail: 'https://img.youtube.com/vi/ZBbXUR9PVzw/hqdefault.jpg',
+    category: 'Adventure'
   }
+];
+
+get filteredVideos() {
+  return this.videos.filter(video =>
+    (this.selectedCategory === 'All' || video.category === this.selectedCategory) &&
+    video.title.toLowerCase().includes(this.searchTerm.toLowerCase())
+  );
+}
+
+openModal(video: any) {
+  const autoplayUrl = video.url.includes('?') ? video.url + '&autoplay=1' : video.url + '?autoplay=1';
+  this.selectedVideo = { ...video, url: autoplayUrl };
+  this.showModal = true;
+}
+
+closeModal() {
+  this.showModal = false;
+  this.selectedVideo = null;
+}
+
+
 }
