8099899fbcddaa90a29416308f381fe657dbe602
TrezaLuTawng <sabawlutawng.24@gmail.com>
2025-09-17 14:26:38 +0630

    answer

--- src/app/pages/assignment/answer/answer.component.ts
+++ src/app/pages/assignment/answer/answer.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import bootstrap from '../../../../main.server';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormsModule } from '@angular/forms';
+import { AnswerserviceService, AssignmentAnswerDto, AssignmentQuestionDto } from './answerservice.service';
 
 @Component({
   selector: 'app-answer',
@@ -15,33 +16,103 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './answer.component.html',
   styleUrl: './answer.component.scss'
 })
-export class AnswerComponent {
 
+export class AnswerComponent implements OnInit {
 
+  submissions: any[] = [];
   currentPages: number = 1;
+  selectedSubmissionId!: string;
 
-  setPage(pageNumber: number): void {
-    this.currentPages = pageNumber;
+  questions: AssignmentQuestionDto[] = [];
+  answers: { [questionId: string]: any } = {};
+
+
+  constructor(private AnswerserviceService: AnswerserviceService,private fb: FormBuilder) {
 
   }
 
+  setPage(pageNumber: number): void {
+    this.currentPages = pageNumber;
+  }
+
+  ngOnInit(): void {
+    this.getAllData();
+  }
+
+  getAllData(): void {
+    this.AnswerserviceService.getallsubmission().subscribe({
+      next: (res) => {
+        this.submissions = res;
+        console.log("All Submissions:", this.submissions);
+      },
+      error: (err) => {
+        console.error("Error fetching submissions:", err);
+      }
+    });
+  }
+
+// Map of answers: questionId -> selected option(s)
+//answers: { [questionId: string]: any } = {};
+
+// MCQ checkbox change handler
+onCheckboxChange(event: any, questionId: string) {
+  if (!this.answers[questionId]) {
+    this.answers[questionId] = [];
+  }
+
+  const checked = event.target.checked;
+  const value = event.target.value;
+
+  if (checked) {
+    this.answers[questionId].push(value);
+  } else {
+    const index = this.answers[questionId].indexOf(value);
+    if (index > -1) this.answers[questionId].splice(index, 1);
+  }
+}
+
+
+
+viewDetails(submissionId: string) {
+  this.AnswerserviceService.getQuestionsWithAnswers(submissionId).subscribe({
+    next: (res) => {
+      this.questions = res;
+
+      this.questions.forEach(q => {
+        if (q.questionType === 'truefalse') {
+          // Map first answerText as boolean
+          this.answers[q.questionId] = q.answers?.[0]?.answerText === 'true';
+        } else if (q.questionType === 'mcq') {
+          // Map selected optionIds
+          this.answers[q.questionId] = q.answers?.map(a => a.selectedOptionId) || [];
+        }
+      });
+
+      this.currentPages = 2;
+    },
+    error: (err) => console.error(err)
+  });
+}
+
+}
+
   // Answer Detail
 
-  questions = [
-    { id: 'q1', text: 'The Earth is flat.', type: 'truefalse', correctAnswer: 'false' },
-    { id: 'q2', text: 'Which of the following are programming languages?', type: 'mcq',
-      options: ['Python', 'HTML', 'C++', 'Photoshop'], correctAnswer: ['Python', 'C++'] },
-      { id: 'q3', text: 'The Earth is flat.', type: 'truefalse', correctAnswer: 'false' },
-      { id: 'q4', text: 'Which of the following are programming languages?', type: 'mcq',
-      options: ['Python', 'HTML', 'C++', 'Photoshop'], correctAnswer: ['Python', 'C++'] },
-  ];
+  // questions = [
+  //   { id: 'q1', text: 'The Earth is flat.', type: 'truefalse', correctAnswer: 'false' },
+  //   { id: 'q2', text: 'Which of the following are programming languages?', type: 'mcq',
+  //     options: ['Python', 'HTML', 'C++', 'Photoshop'], correctAnswer: ['Python', 'C++'] },
+  //     { id: 'q3', text: 'The Earth is flat.', type: 'truefalse', correctAnswer: 'false' },
+  //     { id: 'q4', text: 'Which of the following are programming languages?', type: 'mcq',
+  //     options: ['Python', 'HTML', 'C++', 'Photoshop'], correctAnswer: ['Python', 'C++'] },
+  // ];
 
-  answers: any = {
-    q1: 'true',
-    q2: ['Python', 'HTML'],
-    q3: 'false',
-    q4: ['C++','Photoshop']
-  };
+  // answers: any = {
+  //   q1: 'true',
+  //   q2: ['Python', 'HTML'],
+  //   q3: 'false',
+  //   q4: ['C++','Photoshop']
+  // };
 
   // questionshowModal = false;
   // questionType = '';
@@ -67,4 +138,4 @@ export class AnswerComponent {
   // }
 
 
-}
+
--- src/app/pages/assignment/answer/answerservice.service.ts
+++ src/app/pages/assignment/answer/answerservice.service.ts
@@ -1,9 +1,48 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../../../environments/environment';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+// DTOs
+export interface AssignmentAnswerDto {
+  answerId: string;
+  submissionId: string;
+  questionId: string;
+  answerText: string;
+  selectedOptionId?: string;
+}
+
+export interface AssignmentOptionDto {
+  optionId: string;
+  optionText: string;
+}
+
+export interface AssignmentQuestionDto {
+  questionId: string;
+  assignmentId: string;
+  questionText: string;
+  questionType: string; // 'truefalse' or 'mcq'
+  options?: AssignmentOptionDto[];
+  answers?: AssignmentAnswerDto[];
+}
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class AnswerserviceService {
 
-  constructor() { }
+  private submissionUrl = environment.LMSAPIUrl + 'AssignmentSubmission';
+
+  constructor(private http: HttpClient) { }
+
+  getallsubmission(): Observable <any[]> {
+    return this.http.get<any[]>(`${this.submissionUrl}`);
+  }
+
+  // Get questions with answers by submissionId
+  getQuestionsWithAnswers(submissionId: string): Observable<AssignmentQuestionDto[]> {
+    return this.http.get<AssignmentQuestionDto[]>(`${this.submissionUrl}/${submissionId}/questions-with-answers`);
+  }
+
 }
